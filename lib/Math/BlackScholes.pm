f144592e2a6afc263d8e0d253464b4fb86468184
adrian.candos <adrian.candos@dena.jp>
2013-09-17 17:51:57 +0900

    lunch break stuff

---
+++ lib/Math/BlackScholes.pm
@@ -0,0 +1,10 @@
+package Math::BlackScholes;
+
+use strict;
+use warnings;
+use utf8;
+
+use Math;
+
+
+1;
---
+++ lib/Math/Finance.pm
@@ -0,0 +1,10 @@
+package Math::Finance;
+
+use strict;
+use warnings;
+use utf8;
+
+use Math;
+
+
+1;
